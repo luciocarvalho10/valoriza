a7f7061457bb6544f158ecb077eae0a491888976
Lucio <luciocarvalho10@hotmail.com.br>
2021-09-28 19:24:10 -0400

    criando repositorio para compliment

---
+++ src/repositories/ComplimentsRepositories.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from "typeorm"
+import { Compliment } from "../entities/Compliment"
+
+@EntityRepository(Compliment)
+class ComplimentsRepositories extends Repository<Compliment> {}
+export { ComplimentsRepositories }
