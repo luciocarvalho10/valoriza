c2e6a8956f750a1104e28bf193400f6896847217
Lucio <luciocarvalho10@hotmail.com.br>
2021-09-30 23:37:41 -0400

    add função Exclude e classToPlain

--- src/services/ListUsersService.ts
+++ src/services/ListUsersService.ts
@@ -1,3 +1,4 @@
+import { classToPlain } from "class-transformer"
 import { getCustomRepository } from "typeorm"
 import { UsersRepositories } from "../repositories/UsersRepositories"
 
@@ -7,7 +8,7 @@ class ListUsersService {
 
     const users = await usersRepositories.find()
 
-    return users
+    return classToPlain(users)
   }
 }
 
